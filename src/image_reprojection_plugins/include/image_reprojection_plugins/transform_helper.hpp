e211a16220f058a8bcc86a49d8b1011e014b3a1e
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-05-26 14:25:41 +0900

    use the native opencv function in TransformHelper

--- src/image_reprojection_plugins/include/image_reprojection_plugins/transform_helper.hpp
+++ src/image_reprojection_plugins/include/image_reprojection_plugins/transform_helper.hpp
@@ -13,62 +13,42 @@ class TransformHelper {
 
     virtual ~TransformHelper() {}
 
-    void set(const cv::Matx34f& transform) {
-        // v1 = R * v2 + t
-        // -> v2 = inv(R) * (v1 - t)
-        // -> v2 = inv(R) * v1 - inv(R) * t
-        R_ = transform.get_minor<3, 3>(0, 0);
-        t_ = transform.get_minor<3, 1>(0, 3);
-        R_inv_ = R_.inv();
-        t_inv_ = -R_inv_ * t_;
+    void set(const cv::Matx34f& m) {
+        // set the transform matrix
+        m_ = m;
+
+        // set the inverse matrix
+        cv::Matx44f m44(cv::Matx44f::eye());
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                m44(i, j) = m_(i, j);
+            }
+        }
+        m_inv_ = m44.inv().get_minor<3, 4>(0, 0);
     }
 
-    void set(const tf::Transform& transform) {
+    void set(const tf::Transform& tf_m) {
+        cv::Matx34f cv_m;
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
-                R_(i, j) = transform.getBasis()[i][j];
+                cv_m(i, j) = tf_m.getBasis()[i][j];
             }
-            t_(i) = transform.getOrigin()[i];
+            cv_m(i, 3) = tf_m.getOrigin()[i];
         }
-        R_inv_ = R_.inv();
-        t_inv_ = -R_inv_ * t_;
+        set(cv_m);
     }
 
-    cv::Matx34f get() const {
-        cv::Matx34f transform;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                transform(i, j) = R_(i, j);
-            }
-            transform(i, 3) = t_(i);
-        }
-        return transform;
-    }
+    cv::Matx34f get() const { return m_; }
 
-    void transform(const cv::Mat& src, cv::Mat& dst) const { transform(src, R_, t_, dst); }
+    cv::Matx34f getInverse() const { return m_inv_; }
 
-    void inverseTransform(const cv::Mat& src, cv::Mat& dst) const {
-        transform(src, R_inv_, t_inv_, dst);
-    }
+    void transform(const cv::Mat& src, cv::Mat& dst) const { cv::transform(src, dst, m_); }
+
+    void inverseTransform(const cv::Mat& src, cv::Mat& dst) const { cv::transform(src, dst, m_inv_); }
 
    private:
-    static void transform(const cv::Mat& src, const cv::Matx33f& R, const cv::Matx31f& t,
-                          cv::Mat& dst) {
-        CV_Assert(src.type() == CV_32FC3);
-
-        dst.create(src.size(), src.type());
-        for (int x = 0; x < src.cols; ++x) {
-            for (int y = 0; y < src.rows; ++y) {
-                dst.at<cv::Matx31f>(y, x) = R * src.at<cv::Matx31f>(y, x) + t;
-            }
-        }
-    }
-
-   private:
-    cv::Matx33f R_;
-    cv::Matx31f t_;
-    cv::Matx33f R_inv_;
-    cv::Matx31f t_inv_;
+    cv::Matx34f m_;
+    cv::Matx34f m_inv_;
 };
 }
 #endif  // IMAGE_REPROJECTION_PLUGINS_TRANSFORM_HELPER_HPP
