f0db46b95d1ab0ad017e73738b91add430571bcd
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-05-30 07:27:49 +0900

    add support of extrinsic matrix

--- src/image_reprojection_plugins/include/image_reprojection_plugins/fisheye_projection.hpp
+++ src/image_reprojection_plugins/include/image_reprojection_plugins/fisheye_projection.hpp
@@ -6,6 +6,7 @@
 
 #include <cv_extension/calib3d/calib3d.hpp>
 #include <image_reprojection/projection_interface.hpp>
+#include <image_reprojection_plugins/transform_helper.hpp>
 #include <ros/console.h>
 #include <utility_headers/param.hpp>
 
@@ -30,33 +31,46 @@ class FisheyeProjection : public image_reprojection::ProjectionInterface {
         uhp::getRequired(pnh, "camera_matrix", camera_matrix_);
         dist_coeffs_ = uhp::param(pnh, "dist_coeffs", std::vector<double>(4, 0.));
         min_z_ = std::cos(uhp::param<double>(pnh, "field_of_view", M_PI) / 2.);
+        transform_.set(uhp::param<cv::Matx34f>(pnh, "extrinsic_matrix", cv::Matx34f::eye()));
 
         ROS_INFO_STREAM(getName() << " has been initialized");
     }
 
     virtual void onProject(const cv::Mat& src, cv::Mat& dst, cv::Mat& mask) {
-        cv::fisheye::projectPoints(src.reshape(3, src.total()), dst, cv::Vec3d::all(0.),
-                                   cv::Vec3d::all(0.), camera_matrix_, dist_coeffs_);
-        dst = dst.reshape(2, src.size().height);
+        // transform source points into the camera coordinate
+        cv::Mat cam;
+        transform_.transform(src, cam);
 
-        mask.create(src.size(), CV_8UC1);
+        // project 3D points in the camera coordinate into the 2D image coordinate
+        cv::fisheye::projectPoints(cam.reshape(3, cam.total()), dst, cv::Vec3d::all(0.),
+                                   cv::Vec3d::all(0.), camera_matrix_, dist_coeffs_);
+        dst = dst.reshape(2, cam.size().height);
+
+        // mask points out of the projection cone
+        mask.create(dst.size(), CV_8UC1);
         for (int x = 0; x < mask.size().width; ++x) {
             for (int y = 0; y < mask.size().height; ++y) {
-                const cv::Point3f& p(src.at<cv::Point3f>(y, x));
+                const cv::Point3f& p(cam.at<cv::Point3f>(y, x));
                 mask.at<unsigned char>(y, x) = (p.z / cv::norm(p) > min_z_) ? 1 : 0;
             }
         }
     }
 
     virtual void onReproject(const cv::Mat& src, cv::Mat& dst, cv::Mat& mask) {
-        cv::fisheye::undistortPointsTo3D(src.reshape(2, src.total()), dst, camera_matrix_,
+        // reproject 2D points in the image coordinate to the camera coordinate
+        cv::Mat cam;
+        cv::fisheye::undistortPointsTo3D(src.reshape(2, src.total()), cam, camera_matrix_,
                                          dist_coeffs_);
-        dst = dst.reshape(3, src.size().height);
+        cam = cam.reshape(3, src.size().height);
 
-        mask.create(src.size(), CV_8UC1);
+        // transform the points in the camera coordinate into the destination coordinate
+        transform_.inverseTransform(cam, dst);
+
+        // mask points out of the projection cone
+        mask.create(dst.size(), CV_8UC1);
         for (int x = 0; x < mask.size().width; ++x) {
             for (int y = 0; y < mask.size().height; ++y) {
-                const cv::Point3f& p(dst.at<cv::Point3f>(y, x));
+                const cv::Point3f& p(cam.at<cv::Point3f>(y, x));
                 mask.at<unsigned char>(y, x) = (p.z / cv::norm(p) > min_z_) ? 1 : 0;
             }
         }
@@ -67,6 +81,7 @@ class FisheyeProjection : public image_reprojection::ProjectionInterface {
     cv::Matx33d camera_matrix_;
     std::vector<double> dist_coeffs_;
     double min_z_;
+    TransformHelper transform_;
 };
 }
 
--- src/image_reprojection_plugins/include/image_reprojection_plugins/standard_projection.hpp
+++ src/image_reprojection_plugins/include/image_reprojection_plugins/standard_projection.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include <image_reprojection/projection_interface.hpp>
+#include <image_reprojection_plugins/transform_helper.hpp>
 #include <ros/console.h>
 #include <utility_headers/param.hpp>
 
@@ -35,19 +36,26 @@ class StandardProjection : public image_reprojection::ProjectionInterface {
         frame_.height = std::tan(fov(1) / 2.) * 2.;
         frame_.x = -frame_.width / 2.;
         frame_.y = -frame_.height / 2.;
+        transform_.set(uhp::param<cv::Matx34f>(pnh, "extrinsic_matrix", cv::Matx34f::eye()));
 
         ROS_INFO_STREAM(getName() << " has been initialized");
     }
 
     virtual void onProject(const cv::Mat& src, cv::Mat& dst, cv::Mat& mask) {
-        cv::projectPoints(src.reshape(3, src.total()), cv::Vec3d::all(0.), cv::Vec3d::all(0.),
-                          camera_matrix_, dist_coeffs_, dst);
-        dst = dst.reshape(2, src.size().height);
+        // transform source points into the camera coordinate
+        cv::Mat cam;
+        transform_.transform(src, cam);
 
-        mask.create(src.size(), CV_8UC1);
+        // project 3D points in the camera coordinate into the 2D image coordinate
+        cv::projectPoints(cam.reshape(3, cam.total()), cv::Vec3d::all(0.), cv::Vec3d::all(0.),
+                          camera_matrix_, dist_coeffs_, dst);
+        dst = dst.reshape(2, cam.size().height);
+
+        // mask points out of the projection frame
+        mask.create(dst.size(), CV_8UC1);
         for (int x = 0; x < mask.size().width; ++x) {
             for (int y = 0; y < mask.size().height; ++y) {
-                const cv::Point3f& p(src.at<cv::Point3f>(y, x));
+                const cv::Point3f& p(cam.at<cv::Point3f>(y, x));
                 mask.at<unsigned char>(y, x) =
                     frame_.contains(cv::Point2f(p.x / p.z, p.y / p.z)) ? 1 : 0;
             }
@@ -55,21 +63,25 @@ class StandardProjection : public image_reprojection::ProjectionInterface {
     }
 
     virtual void onReproject(const cv::Mat& src, cv::Mat& dst, cv::Mat& mask) {
-        // 2D image points -> 2D object points
-        cv::Mat dst2d;
-        cv::undistortPoints(src.reshape(2, src.total()), dst2d, camera_matrix_, dist_coeffs_);
+        // reproject 2D points in the image coordinate to the camera coordinate
+        cv::Mat cam_2d;
+        cv::undistortPoints(src.reshape(2, src.total()), cam_2d, camera_matrix_, dist_coeffs_);
 
-        // 2D object points + z-channel -> 3D object points
-        dst.create(src.size(), CV_32FC3);
+        // add z-channel to the points in the camera coordinate
+        cv::Mat cam(cam_2d.size(), CV_32FC3);
         // copy xy-channels
-        dst2d.reshape(1, dst2d.total()).copyTo(dst.reshape(1, dst.total()).colRange(0, 2));
+        cam_2d.reshape(1, cam_2d.total()).copyTo(cam.reshape(1, cam.total()).colRange(0, 2));
         // fill z-channel to 1.0
-        dst.reshape(1, dst.total()).col(2).setTo(1.);
+        cam.reshape(1, cam.total()).col(2).setTo(1.);
 
-        mask.create(src.size(), CV_8UC1);
+        // transform the points in the camera coordinate into the destination coordinate
+        transform_.inverseTransform(cam, dst);
+
+        // mask points out of the projection frame
+        mask.create(dst.size(), CV_8UC1);
         for (int x = 0; x < mask.size().width; ++x) {
             for (int y = 0; y < mask.size().height; ++y) {
-                const cv::Point3f& p(dst.at<cv::Point3f>(y, x));
+                const cv::Point3f& p(cam.at<cv::Point3f>(y, x));
                 mask.at<unsigned char>(y, x) =
                     frame_.contains(cv::Point2f(p.x / p.z, p.y / p.z)) ? 1 : 0;
             }
@@ -80,6 +92,7 @@ class StandardProjection : public image_reprojection::ProjectionInterface {
     cv::Matx33d camera_matrix_;
     std::vector<double> dist_coeffs_;
     cv::Rect_<float> frame_;
+    TransformHelper transform_;
 };
 }
 
