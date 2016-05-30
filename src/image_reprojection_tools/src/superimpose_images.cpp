394c82306dc505a23920b1439547b7b87289fac4
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-05-30 09:05:54 +0900

    add exception handling

--- src/image_reprojection_tools/src/superimpose_images.cpp
+++ src/image_reprojection_tools/src/superimpose_images.cpp
@@ -1,11 +1,14 @@
+#include <stdexcept>
+
 #include <image_reprojection_tools/image_superimposition.hpp>
+#include <ros/console.h>
 #include <ros/init.h>
 #include <ros/names.h>
 #include <ros/spinner.h>
 #include <ros/this_node.h>
 #include <utility_headers/param.hpp>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     namespace irt = image_reprojection_tools;
     namespace uhp = utility_headers::param;
 
@@ -14,11 +17,15 @@ int main(int argc, char *argv[]) {
     ros::V_string args;
     ros::removeROSArgs(argc, argv, args);
 
-    irt::ImageSuperimposition node;
-    node.init(ros::this_node::getName(), ros::names::getRemappings(), args);
+    try {
+        irt::ImageSuperimposition node;
+        node.init(ros::this_node::getName(), ros::names::getRemappings(), args);
 
-    ros::MultiThreadedSpinner spinner(2);
-    spinner.spin();
+        ros::MultiThreadedSpinner spinner(2);
+        spinner.spin();
+    } catch (const std::exception& ex) {
+        ROS_ERROR_STREAM(ex.what());
+    }
 
     return 0;
 }
