fc9f222f6b716e59a73d5cc4302726659a4a0f3f
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-08-03 06:30:55 +0900

    remove old cpp files no longer compiled

--- image_reprojection_plugins/src/fisheye_projection_plugin.cpp
+++
@@ -1,6 +0,0 @@
-#include <image_reprojection/projection_interface.hpp>
-#include <image_reprojection_plugins/fisheye_projection.hpp>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(image_reprojection_plugins::FisheyeProjection,
-                       image_reprojection::ProjectionInterface);
--- image_reprojection_plugins/src/rotation_transform_plugin.cpp
+++
@@ -1,6 +0,0 @@
-#include <image_reprojection/transform_interface.hpp>
-#include <image_reprojection_plugins/rotation_transform.hpp>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(image_reprojection_plugins::RotationTransform,
-                       image_reprojection::TransformInterface);
