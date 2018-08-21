a65d5c9aa2894f6d6543dea2e01c8c5dde23714f
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-08-22 07:36:04 +0900

    ImageReprojection class as nodelet plugin

---
+++ image_reprojection/src/image_reprojection.cpp
@@ -0,0 +1,5 @@
+#include <image_reprojection/image_reprojection.hpp>
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(image_reprojection::ImageReprojection, nodelet::Nodelet);
