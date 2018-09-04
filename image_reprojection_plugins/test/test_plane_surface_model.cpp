4a44c53dc9ee40ab9e9a662bc1b92194d5c63283
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-09-05 07:16:42 +0900

    refactor test of PlaneSurfaceModel

--- image_reprojection_plugins/test/test_plane_surface_model.cpp
+++ image_reprojection_plugins/test/test_plane_surface_model.cpp
@@ -7,33 +7,14 @@
 
 #include <opencv2/core/core.hpp>
 
+#include "random.hpp"
+
 namespace irp = image_reprojection_plugins;
 
-// the global random number generator
-cv::RNG g_rng(std::time(NULL));
-
-cv::Vec3f randomPoint() {
-  return cv::Vec3f(g_rng.uniform(-1., 1.), g_rng.uniform(-1., 1.), g_rng.uniform(-1., 1.));
-}
-
-cv::Mat randomPoints(const cv::Size &size) {
-  cv::Mat points(size, CV_32FC3);
-  for (int x = 0; x < size.width; ++x) {
-    for (int y = 0; y < size.height; ++y) {
-      points.at< cv::Vec3f >(y, x) = randomPoint();
-    }
-  }
-  return points;
-}
-
-irp::PlaneStamped toPlane(const cv::Vec3f &point, const cv::Vec3f &normal) {
+irp::PlaneStamped toPlaneMsg(const cv::Vec3f &point, const cv::Vec3f &normal) {
   irp::PlaneStamped plane;
-  plane.point.x = point[0];
-  plane.point.y = point[1];
-  plane.point.z = point[2];
-  plane.normal.x = normal[0];
-  plane.normal.y = normal[1];
-  plane.normal.z = normal[2];
+  plane.point = toPointMsg(point);
+  plane.normal = toPointMsg(normal);
   return plane;
 }
 
@@ -41,13 +22,13 @@ TEST(PlaneSurfaceModel, randomIntersection) {
   // initialize tested model with random plane
   irp::PlaneSurfaceModel model;
   model.init("plane", ros::M_string(), ros::V_string());
-  const cv::Vec3f point(randomPoint()), normal(randomPoint());
-  model.update(toPlane(point, normal));
+  const cv::Vec3f point(randomPoint(-1., 1.)), normal(randomNonZeroPoint(-1., 1.));
+  model.update(toPlaneMsg(point, normal));
 
   // calculate intersections using tested model
   const cv::Size size(100, 100);
-  const cv::Vec3f ray_origin(randomPoint());
-  const cv::Mat ray_direction(randomPoints(size));
+  const cv::Vec3f ray_origin(randomPoint(-1., 1.));
+  const cv::Mat ray_direction(randomNonZeroPoints(size, -1., 1.));
   cv::Mat intersection, mask(cv::Mat::ones(size, CV_8UC1));
   model.intersection(ray_origin, ray_direction, intersection, mask);
 
@@ -57,7 +38,7 @@ TEST(PlaneSurfaceModel, randomIntersection) {
       if (mask.at< unsigned char >(y, x) != 0) {
         // intersection point is on the plane
         const cv::Vec3f i(intersection.at< cv::Vec3f >(y, x));
-        EXPECT_NEAR(normal.dot(intersection.at< cv::Vec3f >(y, x) - point), 0., 0.001);
+        EXPECT_NEAR(normal.dot(i - point), 0., 0.001 * cv::norm(normal) * cv::norm(i - point));
 
         // ray direction vector points intersection point
         const cv::Vec3f d(ray_direction.at< cv::Vec3f >(y, x));
@@ -73,7 +54,7 @@ TEST(PlaneSurfaceModel, randomIntersection) {
 
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "test_mesh_surface_model");
+  ros::init(argc, argv, "test_plane_surface_model");
   ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
